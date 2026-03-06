70f27c8377662b804e0f669c687d3a7926e43f91
Jadesola Olutola <upperlowlance@outlook.com>
2026-03-06 11:15:01 +0300

    Установил Tailwind Локально

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/templates/**/*.{html,js}",
+    "./app/static/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
